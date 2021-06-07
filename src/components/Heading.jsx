9f9eaab568cdbd8a62b03b24c0ba10877a74d6f7
Luis Lopez <lglvluislopez@gmail.com>
2021-06-06 23:50:01 -0500

    creating heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function heading() {
+    return (
+        <header className="page-header bg-dark">
+            <h1>hoa</h1>
+        </header>
+    )
+}
+
+export default heading
